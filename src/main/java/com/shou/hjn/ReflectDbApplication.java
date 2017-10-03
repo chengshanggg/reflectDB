057b652344c6a8b96c9776a80ac2c6928b42410f
Hujinnan <hjn9504@gmail.com>
2017-10-03 17:30:29 +0800

    Initial commit

---
+++ src/main/java/com/shou/hjn/ReflectDbApplication.java
@@ -0,0 +1,12 @@
+package com.shou.hjn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReflectDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReflectDbApplication.class, args);
+	}
+}
